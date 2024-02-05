99ad2136c3c36117f58d743e41c0ae23e9f4c665
jiaxiangu <gujiaxian1997@gmail.com>
2024-02-05 14:24:08 +0900

    change backend port

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,7 +1,8 @@
 const {setupServer} = require("./server.js");
+require('dotenv').config({path: '../../.env'});
 
 const server = setupServer();
-const PORT = 8080;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
     console.log("Server listening on Port: ", PORT);
